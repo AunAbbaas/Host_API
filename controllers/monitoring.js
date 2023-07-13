55218871b1a8c4aab17f4eb1eb48b373ff7bebc1
Aun Abbas <aaun0019@gmail.com>
2023-07-13 18:41:44 +0500

    new push pushed

--- controllers/monitoring.js
+++ controllers/monitoring.js
@@ -1,32 +1,34 @@
 const express = require("express");
 const os = require("os");
 const process = require("process");
-const IP = require('ip');
+const IP = require("ip");
 
 const app = express();
 app.use(express.json());
 
 const ipAddress = IP.address();
 
-const monitoring =  (req, res) => {
-  const serverDetails = {
-    Host : req.get('host'),
-    url : req.url,
-    requestType:req.method,
-    protocols:req.protocol,
-    hostname: os.hostname(),
-    platform: os.platform(),
-    uptime: os.uptime(),
-    loadavg: os.loadavg(),
-    release: os.release(),
-    arch: os.arch(),
-    totalMemory: os.totalmem(),
-    freeMemory: os.freemem(),
-    cpuUsage: process.cpuUsage(),
-    memoryUsage: process.memoryUsage(),
-    IpAddress:ipAddress
-  };
-  res.json(serverDetails)
-}
+const monitoring = (req, res) => {
+  const serverDetails = [
+    {
+      Host: req.get("host"),
+      url: req.url,
+      requestType: req.method,
+      protocols: req.protocol,
+      hostname: os.hostname(),
+      platform: os.platform(),
+      uptime: os.uptime(),
+      loadavg: os.loadavg(),
+      release: os.release(),
+      arch: os.arch(),
+      totalMemory: os.totalmem(),
+      freeMemory: os.freemem(),
+      cpuUsage: process.cpuUsage(),
+      memoryUsage: process.memoryUsage(),
+      IpAddress: ipAddress,
+    },
+  ];
+  res.json(serverDetails);
+};
 
-module.exports = {monitoring}
+module.exports = { monitoring };
